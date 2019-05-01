5c75a5be9722ace540937e406a1ad4b8feb5ae44
gisoo <gisoo@DESKTOP-0KPDAF3>
2019-05-01 22:24:03 +0430

    Add project files.

---
+++ WindowsFormsApplication04/Form1.cs
@@ -0,0 +1,28 @@
+﻿namespace WindowsFormsApplication04
+{
+	public partial class Form1 : System.Windows.Forms.Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_MouseEnter(object sender, System.EventArgs e)
+		{
+			System.Windows.Forms.Button currentButton = sender as System.Windows.Forms.Button;
+			if (currentButton != null)
+			{
+				currentButton.BackColor = System.Drawing.Color.Yellow;
+			}
+		}
+
+		private void Button_MouseLeave(object sender, System.EventArgs e)
+		{
+			System.Windows.Forms.Button currentButton = sender as System.Windows.Forms.Button;
+			if (currentButton != null)
+			{
+				currentButton.BackColor = System.Drawing.Color.LightGray;
+			}
+		}
+	}
+}
